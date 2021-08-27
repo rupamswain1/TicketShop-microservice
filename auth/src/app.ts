6a83f9779f56689adeaef8edd607e10f36ca3602
Rupam Swain <rupamswain1@gmail.com>
2021-08-28 00:46:48 +0530

    routes added

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 import {json} from 'body-parser';
-
+import { currentUserRouter } from './routes/current-user';
 const app=express();
 app.use(json());
 
-app.get('/api/users/',(req,res,next)=>{
-    res.json({message:"success"})
-})
+app.use(currentUserRouter);
 
 app.listen(4000,()=>{
     console.log(">>>>>Auth Service Listining on Port: 4000<<<<<<<")
